36373e3aebbaedd41eae4d9cc29f90d29adcfb42
HarrisonHuH <1436179815@qq.com>
2026-04-24 17:18:05 +0800

    a

--- 01_测试apikey的使用.py
+++ 01_测试apikey的使用.py
@@ -3,26 +3,16 @@ import os
 
 client = OpenAI(
     # 如果没有配置环境变量，请用阿里云百炼API Key替换：api_key="sk-xxx"
-    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    api_key="sk-c8db990f3ad74d0f918c1411dd57cf33",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-messages = [{"role": "user", "content": "你是谁"}]
+messages = [{"role":"system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": "你最喜欢nba的谁"}]
 completion = client.chat.completions.create(
     model="qwen3-max",  # 您可以按需更换为其它深度思考模型
     messages=messages,
-    extra_body={"enable_thinking": True},
     stream=True
 )
-is_answering = False  # 是否进入回复阶段
-print("\n" + "=" * 20 + "思考过程" + "=" * 20)
 for chunk in completion:
-    delta = chunk.choices[0].delta
-    if hasattr(delta, "reasoning_content") and delta.reasoning_content is not None:
-        if not is_answering:
-            print(delta.reasoning_content, end="", flush=True)
-    if hasattr(delta, "content") and delta.content:
-        if not is_answering:
-            print("\n" + "=" * 20 + "完整回复" + "=" * 20)
-            is_answering = True
-        print(delta.content, end="", flush=True)
+    print(chunk.choices[0].delta.content, end="", flush=True)
